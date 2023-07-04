af4c86c3db80cb5a072cecbd7eb638ef54061136
JackBJ23 <91531374+JackBJ23@users.noreply.github.com>
2023-07-04 23:17:00 +0200

    Update fcgan_mnist.py

--- fcgan_mnist.py
+++ fcgan_mnist.py
@@ -6,10 +6,46 @@ import torchvision.datasets as datasets
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 from torch.utils.tensorboard import SummaryWriter  # to print to tensorboard
-
 import matplotlib.pyplot as plt
 import torchvision.transforms.functional as TF
+#other for PDiags:
+import math
+import numpy as np
+import scipy
+import matplotlib.animation as animation
 
+## install and import the persistence diagram constructor:
+!pip install git+https://github.com/HuPBA/fast_zero_dimensional_persistence_diagrams
+import zero_persistence_diagram
+
+## next 3 fctns for topological loss:
+def get_indices_from_condensed_index(condensed_index, number_of_points):
+    b = 1 - (2 * number_of_points)
+    i = int((-b - math.sqrt(b ** 2 - 8 * condensed_index)) // 2)
+    j = condensed_index + i * (b + i + 2) // 2 + 1
+    return i, j
+
+def compute_persistence_diagram(point_cloud):
+    # Compute euclidean distance matrix for points in the point cloud
+    distance_matrix = torch.cdist(point_cloud, point_cloud, p=2)
+    # Compute the persistence diagram without backprop
+    with torch.no_grad():
+        condensed_distance_matrix = scipy.spatial.distance.squareform(distance_matrix.detach(), checks=False)
+        pd, condensed_pairs = zero_persistence_diagram.zero_persistence_diagram_by_single_linkage_algorithm(
+            condensed_distance_matrix)
+    pairs = torch.tensor([get_indices_from_condensed_index(condensed_index, point_cloud.shape[0]) for condensed_index in
+                          condensed_pairs])
+    # Filter the distance matrix to have the pairs we want
+    pd_according_to_pairs = distance_matrix[pairs[:, 0], pairs[:, 1]]
+    return pd_according_to_pairs
+
+def loss_topo(point_cloud):
+    pd_according_to_pairs = compute_persistence_diagram(point_cloud)
+    # Compute the loss. In this case, we minimise the total persistence of the 
+    # persistence diagram.
+    return torch.sum(pd_according_to_pairs)
+
+## FCGAN:
 class Discriminator(nn.Module):
     def __init__(self, in_features):
         super().__init__()
@@ -22,7 +58,6 @@ class Discriminator(nn.Module):
 
     def forward(self, x):
         return self.disc(x)
-
 
 class Generator(nn.Module):
     def __init__(self, z_dim, img_dim):
@@ -61,10 +96,14 @@ loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 opt_disc = optim.Adam(disc.parameters(), lr=lr)
 opt_gen = optim.Adam(gen.parameters(), lr=lr)
 criterion = nn.BCELoss()
+w_topo = 0.5
 step = 0
 
 for epoch in range(num_epochs):
     for batch_idx, (real, _) in enumerate(loader):
+        opt_disc.zero_grad()
+        opt_gen.zero_grad()
+        
         real = real.view(-1, 784).to(device)
         batch_size = real.shape[0]
 
@@ -84,8 +123,14 @@ for epoch in range(num_epochs):
         # where the second option of maximizing doesn't suffer from
         # saturating gradients
         output = disc(fake).view(-1)
-        lossG = criterion(output, torch.ones_like(output))
+        
+        ##ADDED: topological loss:
+        noise_for_pd = torch.randn(100, z_dim).to(device)
+        point_cloud = gen(noise_for_pd).to(device) ## 100 points of z_dim(=64) dimensions
+        
+        lossG = criterion(output, torch.ones_like(output)) + w_topo * loss_topo(point_cloud)
         gen.zero_grad()
+        
         lossG.backward()
         opt_gen.step()
 
