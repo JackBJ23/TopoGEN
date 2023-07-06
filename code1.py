5a1385b995b0dcf9ffc97e8bd797309bac1d1601
JackBJ23 <91531374+JackBJ23@users.noreply.github.com>
2023-07-06 17:52:21 +0200

    Update code1.py

--- code1.py
+++ code1.py
@@ -1,3 +1,19 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from torch.utils.tensorboard import SummaryWriter  # to print to tensorboard
+import matplotlib.pyplot as plt
+import torchvision.transforms.functional as TF
+#other for PDiags:
+import math
+import numpy as np
+import scipy
+import matplotlib.animation as animation
+
 print("hello")
 a = 3
 a = a+1
