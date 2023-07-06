99644a0da30101b762c14aaaedf167e4a8a13717
JackBJ23 <91531374+JackBJ23@users.noreply.github.com>
2023-07-06 19:02:22 +0200

    Create code2.py

---
+++ code2.py
@@ -0,0 +1,66 @@
+#%matplotlib inline
+import argparse
+import os
+import random
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+from torch.utils.data import DataLoader
+from torchvision.utils import make_grid
+
+import torchvision.datasets as dset
+from torchvision import datasets
+
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from IPython.display import HTML
+
+#for dataset:
+!pip install Pillow
+!pip install PIL
+!pip install image
+import PIL.Image as Image
+import zipfile
+
+# Set random seed for reproducibility
+manualSeed = 999
+#manualSeed = random.randint(1, 10000) # use if you want new results
+print("Random Seed: ", manualSeed)
+random.seed(manualSeed)
+torch.manual_seed(manualSeed)
+torch.use_deterministic_algorithms(True) # Needed for reproducible results
+
+!mkdir data_faces && wget https://s3-us-west-1.amazonaws.com/udacity-dlnfd/datasets/celeba.zip
+
+with zipfile.ZipFile("celeba.zip","r") as zip_ref:
+  zip_ref.extractall("data_faces/")
+
+root = 'data_faces/img_align_celeba'
+img_list = os.listdir(root)
+print(len(img_list))
+
+""" data """
+crop_size = 108
+re_size = 64
+offset_height = (218 - crop_size) // 2
+offset_width = (178 - crop_size) // 2
+crop = lambda x: x[:, offset_height:offset_height + crop_size, offset_width:offset_width + crop_size]
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Lambda(crop),
+     transforms.ToPILImage(),
+     transforms.Resize(size=(re_size, re_size), interpolation=Image.BICUBIC),
+     transforms.ToTensor(),
+     transforms.Normalize(mean=[0.5] * 3, std=[0.5] * 3)])
+
+celeba_data = datasets.ImageFolder('./data_faces', transform=transform)
+dataloader = DataLoader(celeba_data, batch_size=batch_size, shuffle=True)
+
+print("Done")
