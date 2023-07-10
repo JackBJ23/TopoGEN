c9244b6476a29adf5f4d1bfda10a974314af3c8e
JackBJ23 <91531374+JackBJ23@users.noreply.github.com>
2023-07-10 14:08:43 +0200

    Update code2.py

--- code2.py
+++ code2.py
@@ -115,11 +115,12 @@ batch, _ = next(iter(dataloader))
 
 # Plot some training images
 real_batch = next(iter(dataloader))
+print(real_batch[0].shape)
 plt.figure(figsize=(8,8))
 plt.axis("off")
 plt.title("Training Images")
 plt.imshow(np.transpose(make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
-plt.savefig('/home-net/jbenarroch/TopoGEN/img1.png')
+plt.savefig('img1.png')
 
 print("Done")
 
