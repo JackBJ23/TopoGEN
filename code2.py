2642658e189455ac0aac3a5f38fefb97ec4051b0
JackBJ23 <91531374+JackBJ23@users.noreply.github.com>
2023-07-10 13:12:53 +0200

    Update code2.py

--- code2.py
+++ code2.py
@@ -33,6 +33,37 @@ random.seed(manualSeed)
 torch.manual_seed(manualSeed)
 #torch.use_deterministic_algorithms(True) # Needed for reproducible results
 
+## parameters:
+
+##DCGAN parameters:
+
+# Number of workers for dataloader
+workers = 2
+# Batch size during training
+batch_size = 128
+# Spatial size of training images. All images will be resized to this
+image_size = 64
+# Number of channels in the training images. For color images this is 3
+nc = 3
+# Size of z latent vector (i.e. size of generator input)
+nz = 100
+# Size of feature maps in generator
+ngf = 64
+# Size of feature maps in discriminator
+ndf = 64
+# Number of training epochs
+num_epochs = 5
+# Learning rate for optimizers
+lr = 0.0002
+# Beta1 hyperparameter for Adam optimizers
+beta1 = 0.5
+# Number of GPUs available. Use 0 for CPU mode.
+ngpu = 1
+# Decide which device we want to run on
+device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+
+## 3rd block:
+
 import zipfile
 
 # Specify the path to the zip file
