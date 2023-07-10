cc70cfc070a405b560562bde18bb4e2fc442bdcd
JackBJ23 <91531374+JackBJ23@users.noreply.github.com>
2023-07-10 16:03:20 +0200

    Update code2.py

--- code2.py
+++ code2.py
@@ -118,8 +118,8 @@ print(real_batch[0].shape)
 plt.figure(figsize=(8,8))
 plt.axis("off")
 plt.title("Training Images")
+plt.savefig('img1.png')
 plt.imshow(np.transpose(make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
-plt.save('img1.png')
 
 print("Done")
 
